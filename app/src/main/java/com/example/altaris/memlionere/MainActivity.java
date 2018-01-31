2b582b05298778638029bb4cbf5c03510280db8d
Valeriu <valeriu@github.com>
2018-01-31 14:14:25 +0200

    Updated UI
    +some scripts about gameplay

--- app/src/main/java/com/example/altaris/memlionere/MainActivity.java
+++ app/src/main/java/com/example/altaris/memlionere/MainActivity.java
@@ -3,17 +3,15 @@ package com.example.altaris.memlionere;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-//        getActionBar().hide();
+        getActionBar().hide();
 
     }
 }
